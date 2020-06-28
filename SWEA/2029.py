608364ae501f484ab7a4d4e80417ebfc345aa659
CY <openprime15@gmail.com>
2020-06-28 19:51:54 +0900

    add 2029

--- SWEA/2029.py
+++ SWEA/2029.py
@@ -3,10 +3,12 @@
 """
 
 import sys
-sys.stdin = open('2029_input.txt')
+
+sys.stdin = open("2029_input.txt")
 
 N = int(input())
 
-for tc in range(1, N+1):
+for tc in range(1, N + 1):
     a, b = map(int, input().split())
-    print(f'#{tc} {a//b} {a%b}')
+    print(f"#{tc} {a//b} {a%b}")
+
